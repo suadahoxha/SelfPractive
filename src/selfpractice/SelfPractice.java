7ff5b7c84e261793b656edd43c4b9709f48e74f2
suadahoxha <apple@apple-MacBook-Pro.local>
2020-06-28 08:54:44 -0400

    dorduncu commit

--- src/selfpractice/SelfPractice.java
+++ src/selfpractice/SelfPractice.java
@@ -6,6 +6,7 @@ public class SelfPractice {
 		System.out.println("GitHub prova");
 		System.out.println("GitHub ikinci prova");
 		System.out.println("GitHub ucuncu prova");
+		System.out.println("GitHub dorduncu prova");
 
 	}
 
